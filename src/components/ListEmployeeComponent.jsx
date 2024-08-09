e20b45844f35196a2aad971e0fe568bc6fdd3849
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-08-09 16:26:58 +0200

    updated the table with better ui

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -9,45 +9,50 @@ class ListEmployeeComponent extends Component {
         }
     };
     componentDidMount() {
-        EmployeeService.getEmployees().then((res) => {
+        EmployeeService.getEmployees()
+        .then((res) => {
             this.setState({employees: res.data})
-        }).catch(error => {
+        })
+        .catch(error => {
             console.log("there is fetching issue with employees");
         });
     }
     render() {
         return (
-            <div className='flex flex-col space-x-12 content-center'>
-                <h2 className='text-center'>Employee List</h2>
-                <div>
-                    <table className="w-full">
-                        <thead className="text-xm">
-                            <tr className='flex space-x-48 border-b'>
-                                <th>Employee First Name</th>
-                                <th>Employee Last Name</th>
-                                <th>Employee Email Id</th>
-                                <th>Actions</th>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <h2 className="text-center mb-4 text-lg font-semibold">Employee List</h2>
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">Employee First Name</th>
+                            <th scope="col" className="px-6 py-3">Employee Last Name</th>
+                            <th scope="col" className="px-6 py-3">Employee Email Id</th>
+                            <th scope="col" className="px-6 py-3">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.employees.map((employee, index) => (
+                            <tr key={employee.id} className={`odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700`}>
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {employee.firstName}
+                                </th>
+                                <td className="px-6 py-4">
+                                    {employee.lastName}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {employee.emailId}
+                                </td>
+                                <td className="px-6 py-4">
+                                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                           {
-                                this.state.employees.map(
-                                    employee => 
-                                    <tr key={employee.id}>
-                                        <td>{employee.firstName}</td>
-                                        <td>{employee.lastName}</td>
-                                        <td>{employee.emailId}</td>
-                                    </tr>
-
-                                )
-                           } 
-                        </tbody>
-                    </table>
-                </div>
+                        ))}
+                    </tbody>
+                </table>
             </div>
-            
         );
     }
+    
 }
 
 export default ListEmployeeComponent;
