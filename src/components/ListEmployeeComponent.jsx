a2070c39abee02461b2be40c9e61c0f4cce0773f
ZuebeyirEser <zubeyir.eser@icloud.com>
2024-08-09 15:55:02 +0200

    connecting backend with frontend

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import EmployeeService from '../services/EmployeeService';
 
 class ListEmployeeComponent extends Component {
     constructor(props) {
@@ -6,6 +7,13 @@ class ListEmployeeComponent extends Component {
         this.state = {
             employees: []
         }
+    };
+    componentDidMount() {
+        EmployeeService.getEmployees().then((res) => {
+            this.setState({employees: res.data})
+        }).catch(error => {
+            console.log("there is fetching issue with employees");
+        });
     }
     render() {
         return (
@@ -13,14 +21,14 @@ class ListEmployeeComponent extends Component {
                 <h2 className='text-center'>Employee List</h2>
                 <div>
                     <table className="w-full">
-                        <thread className="text-xm">
+                        <thead className="text-xm">
                             <tr className='flex space-x-48 border-b'>
                                 <th>Employee First Name</th>
                                 <th>Employee Last Name</th>
                                 <th>Employee Email Id</th>
                                 <th>Actions</th>
                             </tr>
-                        </thread>
+                        </thead>
                         <tbody>
                            {
                                 this.state.employees.map(
